b721b2026a6de0c78ca0d5952a4d05899ecbddb4
yangchuansheng <yangchuansheng33@gmail.com>
2022-03-27 07:51:01 +0000

    Automated publish: Sun Mar 27 07:51:01 UTC 2022 362165b09e5e79b090160c71cf28ca462ca87dc4

--- cmd/derper/cert.go
+++ cmd/derper/cert.go
@@ -84,7 +84,6 @@ func (m *manualCertManager) TLSConfig() *tls.Config {
 }
 
 func (m *manualCertManager) getCertificate(hi *tls.ClientHelloInfo) (*tls.Certificate, error) {
-	return m.cert, nil
 }
 
 func (m *manualCertManager) HTTPHandler(fallback http.Handler) http.Handler {
